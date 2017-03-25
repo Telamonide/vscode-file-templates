dd6a6c319ac63e837ce7d802528f80f13b764f0d
Oleksandr Osipenko <emepostbox@yandex.ru>
2017-03-25 21:55:20 +0200

    Add tokens: ${EMAIL}, ${LICENSE}, ${FILE}, ${YEAR}. Token ${DATE} return date as locale date string. Fixed creating new file from context menu on Windows OS.

---
+++ templates/Python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        ${FILE}
+# Purpose:
+#
+# Author:      ${AUTHOR}
+#
+# Created:     ${DATE}     
+# Copyright:   (c) ${AUTHOR} ${YEAR}
+# License:     ${LICENSE}
+#-------------------------------------------------------------------------------
+#  Copyright ${YEAR} ${AUTHOR} <${EMAIL}>
+#
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
